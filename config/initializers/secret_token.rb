197555254736ac371a25f6cf23b9eb0d00f8023b
John Grubb Atkinson <john.grubb.atkinson@gmail.com>
2011-10-04 19:07:19 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Atkinsontech::Application.config.secret_token = '465116e5466aaee914499a230e29d698097d693d93b61bc3ec6ef774df6b6f70e76c51b510747e29f20bcf998ed855281dc9818ad1f2c46caac7ee76d9750111'
